8ccb32bed299addec701cee61689d327c8f48552
amiablechief <amiablechief@outlook.com>
2018-11-18 22:50:01 -0500

    Cleanup. Handling of different pin 'types'.

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ embed_url = ""
 embed_title = ""
 embed_type = ""
 jump_url = ""
+embed_desc = ""
 
 @client.event
 async def on_ready():
@@ -40,7 +41,7 @@ async def on_message(message):
     if "!pins" in message.content.lower():
         myGuild = client.get_guild(GUILD_ID)
         for textChannel in myGuild.text_channels:
-            await message.channel.send('#'+ textChannel.name)
+            #await message.channel.send('#'+ textChannel.name)
             for pin in await textChannel.pins():
 
                 # Pin ID (never None)
@@ -60,37 +61,44 @@ async def on_message(message):
                     author = pin.author
                     await message.channel.send("author = " + str(author))
 
+                if pin.content:
+                    title = pin.content
+                    await message.channel.send("content = " + str(pin.content))
+
+                if pin.jump_url:
+                        jump_url = pin.jump_url
+                        #await message.channel.send("jump_url = " + str(jump_url))
+
                 # Get embeds from within the Pin
                 if hasattr(pin, "embeds") and len(pin.embeds) > 0:
                     try:
                         for embed in pin.embeds:
                             if hasattr(embed, "title"):
                                 embed_title = embed.title
-                                await message.channel.send("Embedded title = " + str(embed_title))
+                                #await message.channel.send("Embedded title = " + str(embed_title))
                             if hasattr(embed, "url"):
                                 embed_url = embed.url
-                                await message.channel.send("Embedded URL = " + str(embed_url))
+                                #await message.channel.send("Embedded URL = " + str(embed_url))
                             if hasattr(embed, "type"):
                                 embed_type = embed.type
-                                await message.channel.send("Embedded type = " + str(embed_type))
-                                #if str(embed_type).lower()=="video":
-                                reddit.subreddit(sub_reddit).submit(embed_title+" (via "+str(author)+")", selftext="{}".format(embed_url))
-                                print("Submitted pin with ID = ", pinid, " to Reddit")
+                                #await message.channel.send("Embedded type = " + str(embed_type))
+                                if embed_type=="rich":
+                                    embed_desc=embed.description
+                                    reddit.subreddit(sub_reddit).submit(embed_desc[0:50]+"..."+" (via "+str(author)+")", selftext="{}".format(embed_url))
+                                    print("Submitted pin with ID = ", pinid, " to Reddit")
+                                else:
+                                    reddit.subreddit(sub_reddit).submit(embed_title+" (via "+str(author)+")", selftext="{}".format(embed_url))
+                                    print("Submitted pin with ID = ", pinid, " to Reddit")
                     except:
                         AttributeError
+                elif hasattr(pin, "attachments") and len(pin.attachments) > 0:
+                    for attachment in pin.attachments:
+                        #await message.channel.send("Attachment = " + str(attachment.url))
+                        # Post each pin to Reddit
+                        reddit.subreddit(sub_reddit).submit(title+" (via "+str(author)+")", selftext="{}".format(attachment.url))
+                        print("Submitted pin with ID = ", pinid, " to Reddit")
                 else:
-                    if pin.content:
-                        title = pin.content
-                        await message.channel.send("Title = " + str(title))
-                    if pin.jump_url:
-                        jump_url = pin.jump_url
-                        await message.channel.send("jump_url = " + str(jump_url))
-                    if hasattr(pin, "attachments") and len(pin.attachments) > 0:
-                        for attachment in pin.attachments:
-                            await message.channel.send("Attachment = " + str(attachment.url))
-                            # Post each pin to Reddit
-                            reddit.subreddit(sub_reddit).submit(title+" (via "+str(author)+")", selftext="{}".format(attachment.url))
-                            print("Submitted pin with ID = ", pinid, " to Reddit")
-
+                    reddit.subreddit(sub_reddit).submit(title+" (via "+str(author)+")", selftext="")
+                    print("Submitted pin with ID = ", pinid, " to Reddit")
 
 client.run(TOKEN)
